099f05b26af673188447afacf035aa54a2b30f9b
root <root@aws-small.softboysxp.com>
2013-09-22 22:27:30 +0000

    smaller IO test file size

--- run_all.sh
+++ run_all.sh
@@ -14,9 +14,9 @@ for I in `seq 1 $ITER`; do
 	echo -e '\E[37;44m'"\033[1mRun $I/$ITER\033[0m"
 	./syscall_bench -i 10000 | tee -a $LOG_DIR/syscall_bench.log
 	./vm_bench -i 10000 | tee -a $LOG_DIR/vm_bench.log
-	./io_bench -f `mktemp -u` | tee -a $LOG_DIR/io_bench.log
+	./io_bench -f `mktemp -u` -s 262144 | tee -a $LOG_DIR/io_bench.log
 	if [ -b "/dev/xvdb" ]; then
-		./io_bench -f /mnt/io_bench.local | tee -a $LOG_DIR/io_bench_local.log
+		./io_bench -f /mnt/io_bench.local -s 262144 | tee -a $LOG_DIR/io_bench_local.log
 		rm -f /mnt/io_bench.local
 	fi
 	./pthreads_bench -i 10000 | tee -a $LOG_DIR/pthreads_bench.lgo
